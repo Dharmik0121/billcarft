46d0edfd0ead1c180243b6f060739c4de5876a49
Dharmik0121 <104291062+Dharmik0121@users.noreply.github.com>
2025-07-28 17:10:36 +0530

    fileName chnage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import OTP from './pages/OTP'
 import { useDispatch } from 'react-redux'
